33a7ec7987bb03388cec3a694141d8ec874f34ed
BvHand487 <boris.handjiev@gmail.com>
2025-07-02 14:41:44 +0300

    backend spring application created

---
+++ backend/src/main/java/com/example/trading_sim/TradingSimApplication.java
@@ -0,0 +1,15 @@
+package com.example.trading_sim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSimApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(TradingSimApplication.class, args);
+	}
+
+}
